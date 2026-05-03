7978dfba7381934e10559f8f8b4a81a60172a14a
anjalivs990 <anjalivs990@gmail.com>
2026-05-03 12:41:52 +0530

    Fix: ensure Hibernate schema generation by explicitly configuring entity scanning and repository packages

--- user-service/src/main/java/com/wallet/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/wallet/user_service/UserServiceApplication.java
@@ -2,12 +2,15 @@ package com.wallet.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan("com.wallet.user_service.entity")
+@EnableJpaRepositories("com.wallet.user_service.repository")
 @SpringBootApplication
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 }
